b23f9d9d9a797d12d4856049cbb2bd2948a64e89
Betty Schwartz <betty80906@gmail.com>
2015-07-13 23:03:49 -0600

    Initial commit

---
+++ src/Chapter1/UniqueCharacterString.java
@@ -0,0 +1,36 @@
+package Chapter1;
+
+/* Implement an algorithm to determine if an ASCII string contains unique characters. 
+ * This assumes there are only 256 unique characters which might not be true in some 
+ * languages. 
+ * What if you cannot use additional data structures? */
+
+public class UniqueCharacterString {
+	
+	private boolean doesStringContainUniqueCharacters(String testString){
+		if (testString.length() > 256){
+			return false;
+		}
+		boolean [] charSet = new boolean[256];
+		for ( int i = 0; i < testString.length(); i++){
+			int val = testString.charAt(i);
+			if (charSet[val]){ //This one was found previously
+				return false;
+			}
+			charSet[val] = true;
+		}
+		return true;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		UniqueCharacterString uniqueString = new UniqueCharacterString();
+		boolean unique = uniqueString.doesStringContainUniqueCharacters("abcdefc");
+		System.out.println ("Did the string contain unique characters? " + unique);
+		
+
+	}
+
+}
---
+++ src/Chapter1/UniqueCharacterStringTest.java
@@ -0,0 +1,14 @@
+package Chapter1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UniqueCharacterStringTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
