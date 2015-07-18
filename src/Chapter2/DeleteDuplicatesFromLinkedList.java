31aeb5ae4f7814b759fc9bc030aa721224644864
Betty Schwartz <betty80906@gmail.com>
2015-07-18 09:56:02 -0600

    Adding Chapter 2.1

---
+++ src/Chapter2/DeleteDuplicatesFromLinkedList.java
@@ -0,0 +1,38 @@
+package Chapter2;
+
+import java.util.Hashtable;
+import careerCupLibrary.LinkedListNode;
+import careerCupLibrary.AssortedMethods;
+
+/*
+ * Write code to remove duplicates from an unsorted linked list. 
+ */
+
+public class DeleteDuplicatesFromLinkedList {
+	
+	public static void removeDuplicates(LinkedListNode node){
+		Hashtable<Integer, Boolean> table = new Hashtable<Integer, Boolean>();
+		LinkedListNode previous = null;
+		while (node != null){
+			if (table.containsKey(node.data)) {
+				previous.next = node.next;
+			} else {
+				table.put (node.data, true);
+				previous = node;
+			}
+            node = node.next;
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LinkedListNode head = AssortedMethods.randomLinkedList(10, 0, 10);
+		System.out.println(head.printForward());
+		removeDuplicates(head);
+		System.out.println(head.printForward());
+
+	}
+
+}
